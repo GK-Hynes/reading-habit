1eaf3343d573f8542bb65ee242a2be6a2587802c
Gerard Hynes <gkhynes@gmail.com>
2021-07-22 17:10:45 +0100

    Set up FaunaDB utils

---
+++ utils/Fauna.js
@@ -0,0 +1,75 @@
+const faunadb = require("faunadb");
+const faunaClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+const q = faunadb.query;
+
+function convertRefToId(book) {
+  book.id = book.ref.id;
+  delete book.ref;
+  return book;
+}
+
+async function getBooks() {
+  const { data } = await faunaClient.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection("books"))),
+      q.Lambda("ref", q.Get(q.Var("ref")))
+    )
+  );
+  const books = data.map((book) => {
+    book.id = book.ref.id;
+    delete book.ref;
+    return book;
+  });
+  return books;
+}
+
+async function getBookById(id) {
+  const book = await faunaClient.query(q.Get(q.Ref(q.Collection("books"), id)));
+  book.id = book.ref.id;
+  delete book.ref;
+  return book;
+}
+
+async function getBooksByUser(userId) {
+  const { data } = await faunaClient.query(
+    q.Map(
+      q.Paginate(q.Match(q.Index("books_by_user"), userId)),
+      q.Lambda("ref", q.Get(q.Var("ref")))
+    )
+  );
+  const books = data.map((book) => {
+    book.id = book.ref.id;
+    delete book.ref;
+    return book;
+  });
+  return books;
+}
+
+async function createBook(author, title, completed, userId) {
+  return await faunaClient.query(
+    q.Create(q.Collection("books"), {
+      data: { author, title, completed, userId }
+    })
+  );
+}
+
+async function updateBook(id, author, title, completed) {
+  return await faunaClient.query(
+    q.Update(q.Ref(q.Collection("books"), id), {
+      data: { author, title, completed }
+    })
+  );
+}
+
+async function deleteBook(id) {
+  return await faunaClient.query(q.Delete(q.Ref(q.Collection("books"), id)));
+}
+
+module.exports = {
+  createBook,
+  getBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
+  getBooksByUser
+};
